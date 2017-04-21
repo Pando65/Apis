e1b783f2dab985ec6151bcb7c38d58f22151018a
David-Delgadillo <daviddelgadillo.95@gmail.com>
2017-04-21 18:26:21 -0500

    Pruebas iniciales para drag. Drop no funciona

--- scripts/explore-script.js
+++ scripts/explore-script.js
@@ -1,35 +1,31 @@
-/*window.ondragstart = function () { console.log("bofo1"); $("h2").text("hola"); 
-document.getElementsByTagName('h2').innerHTML = "hola"; console.log("bofo");  };
-
 function transparencia(e) {
     $("#chain1 img").css('box-shadow', '0px 0px 5px 1px rgba(0, 140, 186, 0.5)');
+}
+
+function handleDragEnter() {
+    $(this).css('border', '2px dashed #000');
+}
+
+function handleDragLeave(e) {
+    $(this).css('border', '0px dashed #000');
+}
+
+function handleDragEnd() {
+    $("#chain1 img").css('box-shadow', '0px 0px 0px 0px rgba(0, 140, 186, 0.5)');
+}
+
+function handleDrop(e) {
+    $(this).preventDefault();
+    $(this).stopPropagation();
+    $("#moleculas").css('border', '2px dashed #000');
     console.log("bofote");
+    $(this).css('border', '10px dashed #000');
 }
-*/
+
 $(document).ready( function(){
-alert("ohola");
     $("#moleculas").on('dragstart', transparencia);
-    console.log("bofotisimo");
+    $("#chain1 img").on('dragenter', handleDragEnter);
+    $("#chain1 img").on('dragleave', handleDragLeave);
+    $("#chain1 img").on('drop', handleDrop);
+    $("#moleculas").on('dragend', handleDragEnd);
 });
-/*$(document).on('ready', function(){
-    $("#moleculas").draggable();/*
-    $( "#atomos" ).draggable(
-        /*drag: function( event, ui ) {
- 
-        // Keep the left edge of the element
-        // at least 100 pixels from the container
-        ui.position.left = Math.min( 100, ui.position.left );
-        }*/
- /*   );
-
-    $( "#atomos" ).on( "dragstart", function() {
-        $("#chain1 img").css('box-shadow', '0px 0px 5px 1px rgba(0, 140, 186, 0.5)');
-    });*/
-/*
-function handleDragStart(e) {
-  console.log("bofo");
-  //$("#atomos").fadeOut('slow', function(){});  // this / e.target is the source node.
-}
-
-    document.getElementsByTagName("img").addEventListener('mouseover', handleDragStart, false);
-*/
